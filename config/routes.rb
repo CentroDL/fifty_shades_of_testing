6a1087ebd767e6e97df4d1879bedd91c7222505f
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-27 09:41:29 -0500

    passes contract link test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  root "welcome#index"
 
+  # resources :contracts
+  get '/contract' => 'contracts#new'
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
